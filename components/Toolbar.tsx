91dd5837bb7614971e44bc064a63f88ef88adc5b
Diganta-18 <digantab.cse.jisu21@gmail.com>
2026-05-21 01:55:56 +0530

    Change download format to .txt

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -57,12 +57,11 @@ export default function Toolbar({ roomId, isReadOnly }: ToolbarProps) {
   }, [router]);
 
   const handleDownload = useCallback(() => {
-    const ext = getExtensionForLanguage(language);
     const blob = new Blob([code], { type: 'text/plain' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = `codeshare-${roomId.slice(0, 6)}${ext}`;
+    a.download = `codeshare-${roomId}.txt`;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
